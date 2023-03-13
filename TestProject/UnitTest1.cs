e15b94a8caaab72982ab95f7da62c5be2deb66e5
Fidel Rodriguez Jaimez <fidel.rodriguez@axity.com.mx>
2023-03-13 17:35:33 -0600

    Ajuste a Test

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,38 @@
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using WebAppTest.Controllers;
+using System.Linq;
+
+namespace TestProject
+{
+    public class Tests
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestSucces()
+        {
+            var controler = new WeatherForecastController(_logger);
+            var resultado = controler.Get();
+            Assert.IsNotNull(resultado, "Objeto nulo");
+            Assert.IsTrue(resultado.Any(), "Sin elementos");
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestError()
+        {
+            var controler = new WeatherForecastController(_logger);
+            var resultado = controler.Get();
+            Assert.IsNotNull(resultado, "Objeto nulo");
+            Assert.IsTrue(resultado.Any(), "Sin elementos");
+            Assert.IsTrue(resultado.Any(p => p.TemperatureC == 0), "Temperatura cero");
+            Assert.Pass();
+        }
+    }
+}
